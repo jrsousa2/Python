bb820c0d7acc6115229881f4f24ce917954c11d7
Jose Risomar Sousa <jrsousa2@hotmail.com>
2025-05-03 17:03:57 -0300

    Remove

--- Exe Sync/Check workspace folder.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Absolute path of this script
-current_file = os.path.abspath(__file__)
-print("Current file:", current_file)
-
-# Workspace folder (parent of the file)
-workspace_folder = os.path.dirname(current_file)
-print("Workspace folder:", workspace_folder)
