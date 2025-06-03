d5b88ef0f7e23b3e3205de8bb79c4c4db754e07a
Jose Risomar Sousa <jrsousa2@hotmail.com>
2025-06-03 13:04:59 -0300

    Updt

--- Basics/Include code folders.py
+++ Basics/Include code folders.py
@@ -1,8 +1,10 @@
+# ALSO INCLUDE PATH IN SETTINGS.JSON SO LINTING DOESN'T DETECT ISSUES
 import sys
 
 # adding folder to the system path
 sys.path.insert(0, "D:\\Python\\iTunes\\")
 
+# NOT NEEDED IF ADDED TO SETTINGS
 import PL_Read # type: ignore
 
 print(sys.path)
