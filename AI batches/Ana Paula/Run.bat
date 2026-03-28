f8cb554c13b4e6424bce562505877c9640977533
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-03-28 18:46:37 -0300

    Updt

--- AI batches/Ana Paula/Run.bat
+++
@@ -1,3 +0,0 @@
-rem Replaced by a better logic (loop)
-echo. & echo RUNNING BATCH 1 & call Call_AI || (echo. & echo Continuing after crash...)
-echo. & echo RUNNING BATCH 2 & call Call_AI || (echo. & echo Continuing after crash...)
