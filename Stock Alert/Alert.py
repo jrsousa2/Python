022dfbd30ff0186d1648fa60664539863f22f7c7
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-06-03 23:07:15 -0300

    Updt

--- Stock Alert/Alert.py
+++ Stock Alert/Alert.py
@@ -6,12 +6,18 @@ import smtplib
 from email.message import EmailMessage
 from os import environ
 
-Ticker = "SMCI"
-Target = float(environ["SMCI_TARGET"])
+# YOUR E-MAIL
+Email = environ["GMAIL_APP_SMTP_EMAIL"]
 
+# EMAIL ACCT PWD
 APP_PASSWORD = environ["GMAIL_APP_SMTP_PASSWORD"]
 APP_PASSWORD = APP_PASSWORD.replace(" ", "")
 
+# TICKER SYMOL
+Ticker = environ["TICKER1_VALUE"] 
+Target = float(environ["TICKER1_TARGET"])
+
+# CODE
 data = yf.Ticker(Ticker).history(period="1d")
 
 if data.empty:
@@ -23,11 +29,11 @@ if price > Target:
     msg = EmailMessage()
     msg.set_content(f"{Ticker} is above {Target}: {price}")
     msg["Subject"] = Ticker + " Stock Alert"
-    msg["From"] = "jrfsousa2@gmail.com"
-    msg["To"] = "jrfsousa2@gmail.com"
+    msg["From"] = Email 
+    msg["To"] = Email 
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as s:
-        s.login("jrfsousa2@gmail.com", APP_PASSWORD)
+        s.login(Email , APP_PASSWORD)
         s.send_message(msg)
 else:
     print("\nPrice is not above target:",Target,"\n")        
