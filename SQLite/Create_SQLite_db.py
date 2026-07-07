446d139c28a0fbe0d28c494d38574d758631ef3d
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-07-06 22:54:42 -0300

    Updt

---
+++ SQLite/Create_SQLite_db.py
@@ -0,0 +1,14 @@
+# CREATES A SQLITE DB FROM AN EXCEL FILE
+import sqlite3
+import pandas as pd
+
+# Read Excel
+df = pd.read_excel("employees.xlsx")
+
+# Create (or open) SQLite database file
+conn = sqlite3.connect("test_database.db")
+
+# Write DataFrame to a table
+df.to_sql("employees", conn, if_exists="replace", index=False)
+
+conn.close()
--- SQLite/Read_SQLite-pwd.py
+++ SQLite/Read_SQLite-pwd.py
@@ -1,13 +1,17 @@
 import pandas as pd
 from pysqlcipher3 import dbapi2 as sqlite
+from os import environ
 
 db_path = r"C:\Users\JR Sousa\AppData\Roaming\Apple Computer\MobileSync\Backup\00008101-000601C22232001E\Manifest.db"
+
+# YOUR E-MAIL
+pwd = environ["ITUNES_BKUP_PWD"]
 
 conn = sqlite.connect(db_path)
 c = conn.cursor()
 
 # Replace 'your_password_here' with your iTunes backup password
-c.execute("PRAGMA key='My99life';")
+c.execute(f"PRAGMA key='{pwd}';")
 c.execute("PRAGMA cipher_compatibility = 4;")
 
 tables = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';", conn)
