7dd4ce9918bb5b23714bd31d187587be288ece3f
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-07-07 00:37:23 -0300

    Updt

--- SQLite/SqlAlchemy.py
+++ SQLite/SqlAlchemy.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 engine = create_engine("sqlite:///D:/Python/Databases/Main_database.db")
 
-df = pd.read_sql("SELECT * FROM iTunes where ", engine)
+df = pd.read_sql("SELECT * FROM iTunes where Year>2000", engine)
 
 print(df.head())
